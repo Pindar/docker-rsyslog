04e49f2e3fbedfe73a6e7fd012ce011b7a829047
Simon Dittlmann <simon@itnotes.de>
2015-04-03 14:27:34 +0200

    let container start without ES endpoint set

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,7 @@ if ! [[ "$ELASTICSEARCH_HOST" == "" ]]; then
   echo "INFO: set elasticsearch endpoint to $ELASTICSEARCH_HOST"
   sed -i "s/server=\"localhost\"/server=\"$ELASTICSEARCH_HOST\"/" /etc/rsyslog.d/$RSYSLOG_CENTRAL_CONF
 else
-  echo "ERROR: please set the ELASTICSEARCH_HOST environment variable";
-  exit 1;
+  echo "WARNING: $ELASTICSEARCH_HOST environment variable is empty";
 fi
 
 exec /usr/sbin/rsyslogd -n
